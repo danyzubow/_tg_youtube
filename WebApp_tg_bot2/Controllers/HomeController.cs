6bc97393e888c6d79d04f4c8dda91f24ad4d1bf8
Даня Awesome <zubow.dany@gmail.com>
2019-03-16 12:25:43 +0200

    1

--- WebApp_tg_bot2/Controllers/HomeController.cs
+++ WebApp_tg_bot2/Controllers/HomeController.cs
@@ -106,6 +106,7 @@ namespace WebApp_tg_bot2.Controllers
             catch (Exception e)
             {
                 _TgClient.SendTextMessageAsync(new ChatId(227950395), "error");
+
             }
           
             return Ok();
